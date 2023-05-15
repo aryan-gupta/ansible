867c7ea08d660b85339ea41ed6202dd3b0334bd2
Aryan Gupta <me@theguptaempire.net>
2023-05-15 16:02:46 -0400

    Add network config in setup script

--- setup.sh
+++ setup.sh
@@ -1,15 +1,30 @@
 # remount cowspace so we can install ansible and git
 # https://github.com/zxiiro/ansible-arch-install
 
+WIFI_NAME=""
+
 if [ $# -eq 0 ]; then
     echo "No arguments provided"
     echo "./setup.sh HOSTNAME"
     exit 1
 fi
 
+if [ $1 = "boson" ]; then
+	iwctl station wlan0 scan
+	sleep 2
+	iwctl station wlan0 connect $WIFI_NAME
+	sleep 2
+	if ping -c 1 8.8.8.8
+	then
+		echo "Connected"
+	else
+		exit 1
+	fi
+fi
+
 mount -o remount,size=1G /run/archiso/cowspace
 
-pacman -Sy ansible git efibootmgr --needed --noconfirm
+pacman -Sy ansible-core ansible git efibootmgr --needed --noconfirm
 
 ansible-playbook playbook.yml --extra-vars "hostname=$1"
 
