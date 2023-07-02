e3a6c122a1061540c2e5ced88c7b5e33880152eb
Aryan Gupta <me@theguptaempire.net>
2023-07-01 20:01:33 -0400

    Modify reset script for generalization

--- reset.sh
+++ reset.sh
@@ -1,4 +1,4 @@
-HOST="boson"
+HOST="graviton"
 
 sync
 
@@ -10,26 +10,28 @@ umount /mnt -rf
 # lvchange -an /dev/mapper/kvm_lvg01-root
 # vgchange -an kvm_lvg01
 
-lvremove -ff boson_lvg01-home
-lvremove -ff boson_lvg01-root
-vgremove -ff boson_lvg01
-pvremove -y -ff /dev/mapper/boson_crypt
+lvremove -ff "$HOST""_lvg01-home"
+lvremove -ff "$HOST""_lvg01-root"
+vgremove -ff "$HOST""_lvg01"
+pvremove -y -ff "/dev/mapper/""$HOST""_crypt"
 
-dd if=/dev/zero of=/dev/mapper/boson_crypt bs=1M count=16
+dd if=/dev/zero of="/dev/mapper/""$HOST""_crypt" bs=1M count=16
 
-cryptsetup close boson_crypt
-cryptsetup -q luksErase /dev/nvme0n1p2
-dmsetup remove boson_crypt
-dmsetup remove boson_lvg01-home
-dmsetup remove boson_lvg01-root
+cryptsetup close "$HOST""_crypt"
+#cryptsetup -q luksErase /dev/nvme0n1p2
+cryptsetup -q luksErase /dev/sda2
+dmsetup remove "$HOST""_crypt"
+dmsetup remove "$HOST""_lvg01-home"
+dmsetup remove "$HOST""_lvg01-root"
 
-dd if=/dev/zero of=/dev/nvme0n1p2 bs=1M count=32
-dd if=/dev/zero of=/dev/nvme0n1p1 bs=1M count=16
-dd if=/dev/zero of=/dev/nvme0n1  bs=1M count=16
+#dd if=/dev/zero of=/dev/nvme0n1p2 bs=1M count=32
+#dd if=/dev/zero of=/dev/nvme0n1p1 bs=1M count=16
+#dd if=/dev/zero of=/dev/nvme0n1  bs=1M count=16
+dd if=/dev/zero of=/dev/sda  bs=1M count=1024
 
-rm -f /dev/mapper/boson_crypt
-rm -f /dev/nvme0n1p1
-rm -f /dev/nvme0n1p2
+rm -f "/dev/mapper/""$HOST""_crypt"
+#rm -f /dev/nvme0n1p1
+#rm -f /dev/nvme0n1p2
 
 lvmdiskscan
 partprobe
