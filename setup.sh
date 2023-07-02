ae633eb0852e78d282498a85907ccf3b19d0b4cc
Aryan Gupta <me@theguptaempire.net>
2023-07-01 20:48:19 -0400

    Update bootstrap script

--- setup.sh
+++ setup.sh
@@ -1,34 +1,29 @@
 # remount cowspace so we can install ansible and git
 # https://github.com/zxiiro/ansible-arch-install
 
-WIFI_NAME=""
-
 if [ $# -eq 0 ]; then
     echo "No arguments provided"
     echo "./setup.sh HOSTNAME"
     exit 1
 fi
 
-pacman-key --init
-
-if [ $1 = "boson" ]; then
-	iwctl station wlan0 scan
-	sleep 2
-	iwctl station wlan0 connect $WIFI_NAME
-	sleep 2
-	if ping -c 1 8.8.8.8
-	then
-		echo "Connected"
-	else
-		exit 1
-	fi
+if ping -c 1 "8.8.8.8"; then
+    echo "Connected"
+else
+    echo "Please connect to the internet before running build script"
+    exit 1
 fi
+
+# https://stackoverflow.com/questions/369758
+user_password=$(awk -F ' ' '{print $2}' group_vars/all_secret.yml | xargs echo -n)
+
+pacman-key --init
 
 mount -o remount,size=1G /run/archiso/cowspace
 
 pacman -Sy ansible-core ansible git efibootmgr python python-passlib python-jinja python-yaml python-markupsafe --needed --noconfirm
 
-ansible-playbook playbook.yml --extra-vars "hostname=$1"
+ansible-playbook playbook.yml --extra-vars "hostname=$1" --extra-vars "ansible_become_pass=$user_password"
 
 
 # install dotfiles:
