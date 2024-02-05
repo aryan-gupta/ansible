de0aacf00b0b1bc84de1385ad41b3219021bfd40
Aryan Gupta <me@theguptaempire.net>
2024-02-05 16:32:18 -0500

    Prepare for kvm setup

--- reset.sh
+++ reset.sh
@@ -14,33 +14,40 @@ umount /mnt -rf
 # lvchange -an /dev/mapper/kvm_lvg01-root
 # vgchange -an kvm_lvg01
 
-lvremove -ff "$HOST""_lvg01-home"
-lvremove -ff "$HOST""_lvg01-data"
-lvremove -ff "$HOST""_lvg01-root"
-lvremove -ff "$HOST""_lvg01-var"
-vgremove -ff "$HOST""_lvg01"
+LVM="_lv"
+
+lvremove -ff "$HOST""$LVM""-home"
+lvremove -ff "$HOST""$LVM""-data"
+lvremove -ff "$HOST""$LVM""-root"
+lvremove -ff "$HOST""$LVM""-var"
+vgremove -ff "$HOST""$LVM"""
 pvremove -y -ff "/dev/mapper/""$HOST""_crypt"
 
-dd if=/dev/zero of="/dev/mapper/""$HOST""_crypt" bs=1M count=16
+dd if=/dev/zero of="/dev/mapper/""$HOST""_crypt" bs=1M count=32
 
 cryptsetup close "$HOST""_crypt"
 #cryptsetup -q luksErase /dev/nvme0n1p2
 #cryptsetup -q luksErase /dev/nvme0n1p2
 #cryptsetup -q luksErase /dev/sda2
+dmsetup remove "$HOST""$LVM""-home"
+dmsetup remove "$HOST""$LVM""-data"
+dmsetup remove "$HOST""$LVM""-root"
+dmsetup remove "$HOST""$LVM""-var"
 dmsetup remove "$HOST""_crypt"
-dmsetup remove "$HOST""_lvg01-home"
-dmsetup remove "$HOST""_lvg01-data"
-dmsetup remove "$HOST""_lvg01-root"
-dmsetup remove "$HOST""_lvg01-var"
 
-dd if=/dev/zero of=/dev/nvme0n1p2 bs=1M count=32
-dd if=/dev/zero of=/dev/nvme0n1p1 bs=1M count=32
-dd if=/dev/zero of=/dev/nvme0n1  bs=1M count=32
+# dd if=/dev/zero of=/dev/nvme0n1p2 bs=1M count=32
+# dd if=/dev/zero of=/dev/nvme0n1p1 bs=1M count=32
+# dd if=/dev/zero of=/dev/nvme0n1  bs=1M count=32
 #dd if=/dev/zero of=/dev/sda  bs=1M count=1024
+dd if=/dev/zero of=/dev/vda2  bs=1M count=32
+dd if=/dev/zero of=/dev/vda1  bs=1M count=32
+dd if=/dev/zero of=/dev/vda  bs=1M count=32
 
 rm -f "/dev/mapper/""$HOST""_crypt"
-rm -f /dev/nvme0n1p1
-rm -f /dev/nvme0n1p2
+# rm -f /dev/nvme0n1p1
+# rm -f /dev/nvme0n1p2
+rm -f /dev/vda1
+rm -f /dev/vda2
 
 lvmdiskscan
 partprobe
