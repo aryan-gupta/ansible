79726959ebda33f747931bb52078dd1579064cfe
Aryan Gupta <me@theguptaempire.net>
2024-02-11 13:43:09 -0500

    Add ENC var to reset script

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -15,23 +15,24 @@ umount /mnt -rf
 # a future update will move this entire script into ansible so I
 # can support a partial install
 # @TODO remove this
-LVM="$(grep "lvm_postfix" group_vars/all.yml | awk -F ' ' '{print $2}' | head -1 | xargs echo -n)"
+LVM="$(grep "lvm_postfix"   group_vars/all.yml | awk -F ' ' '{print $2}' | head -1 | xargs echo -n)"
+ENC="$(grep "crypt_postfix" group_vars/all.yml | awk -F ' ' '{print $2}' | head -1 | xargs echo -n)"
 lvremove -ff "$HOST""$LVM""-home"
 lvremove -ff "$HOST""$LVM""-data"
 lvremove -ff "$HOST""$LVM""-root"
 lvremove -ff "$HOST""$LVM""-var"
 vgremove -ff "$HOST""$LVM"""
-pvremove -y -ff "/dev/mapper/""$HOST""_crypt"
+pvremove -y -ff "/dev/mapper/""$HOST""$ENC"
 
-dd if=/dev/zero of="/dev/mapper/""$HOST""_crypt" bs=1M count=32
+dd if=/dev/zero of="/dev/mapper/""$HOST""$ENC" bs=1M count=32
 
-cryptsetup close "$HOST""_crypt"
+cryptsetup close "$HOST""$ENC"
 dmsetup remove "$HOST""$LVM""-home"
 dmsetup remove "$HOST""$LVM""-data"
 dmsetup remove "$HOST""$LVM""-root"
 dmsetup remove "$HOST""$LVM""-var"
-dmsetup remove "$HOST""_crypt"
-rm -f "/dev/mapper/""$HOST""_crypt"
+dmsetup remove "$HOST""$ENC"
+rm -f "/dev/mapper/""$HOST""$ENC"
 
 install_disk=$(grep "install_disk" host_vars/$HOST.yml | awk -F ' ' '{print $2}' | head -1 | xargs echo -n)
 part_postfix=$(grep "part_postfix" host_vars/$HOST.yml | awk -F ' ' '{print $2}' | head -1 | xargs echo -n)
